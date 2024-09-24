03920df82962723aa0a05e6f67d75f03b07168f1
Christopher Suhar <suharce@mail.uc.edu>
2024-09-23 23:18:33 -0400

    Lab_04_04_EvenOdd Completed

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int numToExamine;
+        System.out.println("Enter your number please");
+        numToExamine = in.nextInt();
+
+        if(numToExamine % 2 == 0)
+            System.out.println("The number youve entered is even!");
+        else
+            System.out.println("The number youve entered is odd!");
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+    }
